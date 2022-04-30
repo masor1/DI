7a056af061c4580342456d604d3c707101de5f53
masor1 <masor.dev@gmail.com>
2022-04-30 23:09:11 +0300

    start get & inject

--- app/src/main/java/com/masorone/di/example1/Activity.kt
+++ app/src/main/java/com/masorone/di/example1/Activity.kt
@@ -2,22 +2,10 @@ package com.masorone.di.example1
 
 class Activity {
 
-    private val monitor = Monitor()
-    private val storage = Storage()
-    private val memory = Memory()
-    private val processor = Processor()
-    private val computerTower = ComputerTower(
-        storage,
-        memory,
-        processor
-    )
-    private val keyBoard = KeyBoard()
-    private val mouse = Mouse()
+    lateinit var computer: Computer
+    lateinit var keyboard: KeyBoard
 
-    private val computer = Computer(
-        monitor,
-        computerTower,
-        keyBoard,
-        mouse
-    )
+    init {
+        Component().inject(this)
+    }
 }
---
+++ app/src/main/java/com/masorone/di/example1/Component.kt
@@ -0,0 +1,30 @@
+package com.masorone.di.example1
+
+class Component {
+
+    private fun getComputer(): Computer {
+        val monitor = Monitor()
+        val storage = Storage()
+        val memory = Memory()
+        val processor = Processor()
+        val computerTower = ComputerTower(
+            storage,
+            memory,
+            processor
+        )
+        val keyBoard = KeyBoard()
+        val mouse = Mouse()
+
+        return Computer(
+            monitor,
+            computerTower,
+            keyBoard,
+            mouse
+        )
+    }
+
+    fun inject(activity: Activity) {
+        activity.computer = getComputer()
+        activity.keyboard = KeyBoard()
+    }
+}
